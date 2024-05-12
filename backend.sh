0ecfa15d2cc50023718b0e04abb29c7e5296eaab
Khyathi304 <khyathikamireddy04@gmail.com>
2024-05-12 12:46:25 +0100

    expense-shell1

--- backend.sh
+++ backend.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
+source ./common.sh
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+check_root
 
 echo "Please enter DB password:"
 read -s mysql_root_password
--- frontend.sh
+++ frontend.sh
@@ -1,31 +1,8 @@
 #!/bin/bash
 
+source ./common.sh
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2... $R Failure $N"
-        exit 1
-    else
-        echo -e "$2... $G Success $N"
-        fi
-}
-if [ $USERID -ne 0 ]
-then
-echo "Please run the script with root access"
-exit 1
-else
-echo "You are super User"
-fi
+check_root
 
 dnf install nginx -y &>>$LOGFILE
 VALIDATE $? "Installing nginx" 
